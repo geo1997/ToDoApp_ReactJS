1372756b4fb7327ebeacc36ed32cd3a031bf052a
kgeor <kgeorgesilva97@gmail.com>
2020-03-25 11:10:14 +0530

    Updates

---
+++ precache-manifest.0e9dc8537b949d8d46ca31d2795fddc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797576ead9f86d27b8602a52eb7fb21c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d16a29b6d39945287e10",
+    "url": "/static/css/main.b07e9d7a.chunk.css"
+  },
+  {
+    "revision": "3eafeb5a004f19d50993",
+    "url": "/static/js/2.fcfd98d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fcfd98d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16a29b6d39945287e10",
+    "url": "/static/js/main.9e078ed7.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
